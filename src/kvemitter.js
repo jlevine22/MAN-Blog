5f43127561b93d7bc9b55e3eb4f2a14847167605
Josh Levine <josh.levine1@gmail.com>
2016-03-12 15:55:18 -0800

    Cleanup indentation

--- src/kvemitter.js
+++ src/kvemitter.js
@@ -1,42 +1,43 @@
+'use strict';
 var _ = require('lodash');
 var util = require('util');
 var events = require('events');
 
 function KVEmitter(start) {
-    var data = start || {};
+  var data = start || {};
 
-    this.get = function(key) {
-        if (data[key]) {
-            return data[key];
-        }
-        return null;
+  this.get = function(key) {
+    if (data[key]) {
+      return data[key];
     }
+    return null;
+  };
 
-    this.put = function(key, value) {
-        var event = { key: key, value: value };
-        if (data[key]) {
-            event.oldValue = data[key];
-        }
-        data[key] = value;
-        this.emit('put', event);
+  this.put = function(key, value) {
+    var event = { key: key, value: value };
+    if (data[key]) {
+      event.oldValue = data[key];
     }
+    data[key] = value;
+    this.emit('put', event);
+  };
 
-    this.delete = function(key) {
-        if (data[key]) {
-            delete data[key];
-            this.emit('delete', key);
-        }
+  this.delete = function(key) {
+    if (data[key]) {
+      delete data[key];
+      this.emit('delete', key);
     }
+  };
 
-    this.keys = function() {
-        return Object.keys(data);
-    }
+  this.keys = function() {
+    return Object.keys(data);
+  };
 
-    this.forEach = function(iterator) {
-        this.keys().forEach(function(key) {
-            iterator.call(this, data[key], key);
-        });
-    }
+  this.forEach = function(iterator) {
+    this.keys().forEach(function(key) {
+      iterator.call(this, data[key], key);
+    });
+  };
 }
 
 util.inherits(KVEmitter, events.EventEmitter);
