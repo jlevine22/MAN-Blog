1d285013bd54cb7119ee8c4c551fc735c05c1e70
Josh Levine <josh.levine1@gmail.com>
2016-03-27 10:56:41 -0700

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,9 @@ echo "pulling latest version of the code"
 ssh $DEPLOY_USER@$DEPLOY_HOST 'docker pull jlevine22/man-blog'
 
 echo "restarting the container"
-ssh $DEPLOY_USER@$DEPLOY_HOST 'reload_man'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker stop man-blog-web'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker rm man-blog-web'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run --name man-blog-web --restart=always -d -p 3000:3000 -v "$DEPLOY_CACHE_DIR":/cache -v "$DEPLOY_POSTS_DIR":/posts jlevine22/man-blog node /src/src/app.js'
 
 echo "success!"
 
